7e00c2e3eab8d3cb7951d14264d6d06fcdc02c77
balmmva <balmmva@users.noreply.github.com>
2023-01-20 13:45:01 +0100

    Add files via upload
    php error : added single quotes
    jpgraph error : updated to 4.4.1

--- page.concurrentcalls.php
+++ page.concurrentcalls.php
@@ -3,8 +3,8 @@
 if($quietmode && $_REQUEST['ccgraph']) {
 	include_once(dirname(__FILE__).'/concurrentcalls/cc_graph.php');
 } else {
-	$cm =& cronmanager::create($db);
-	$online_updates = $cm->updates_enabled() ? true : false;
+//	$cm =& cronmanager::create($db);
+//	$online_updates = $cm->updates_enabled() ? true : false;
 
 	// check dev site to see if new version of module is available
 	if ($online_updates && $foo = callstatistics_vercheck()) {
@@ -12,6 +12,7 @@ if($quietmode && $_REQUEST['ccgraph']) {
 	}
 	include_once(dirname(__FILE__).'/concurrentcalls/concurrentcalls.php');
 	$module_local = callstatistics_xml2array("modules/callstatistics/module.xml");
-	echo $module_local[module][name]." ver: ".$module_local[module][version];
+	// 20230120 enclose module, name and version with singke quote for compatibility with recent php
+	echo $module_local['module']['name']." ver: ".$module_local['module']['version'];
 }
 ?>
